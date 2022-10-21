37b7d05aacb8af90e0a21155005cf2596aa6d806
EgorKirillov <80296253508@mail.ru>
2022-10-21 18:05:53 +0300

    add: get with filtration and sort

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 // import { connectDb } from './repository/db';
 
-const { Client } = require('pg');
+
+const {Client} = require('pg');
+
 let client = new Client()
+
 export const connectDb = async () => {
   try {
     client = new Client({
@@ -16,10 +19,10 @@ export const connectDb = async () => {
     });
     
     await client.connect();
-    let res =  await client.query('SELECT * FROM main');
+    // let res = await client.query('SELECT * FROM main');
     
     
-    console.log(res);
+    // console.log(res);
     // await client.end();
   } catch (error) {
     console.log(error);
@@ -28,9 +31,8 @@ export const connectDb = async () => {
 
 
 const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
@@ -40,14 +42,104 @@ const startApp = async () => {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
+  
+  
   app.get('/', async (req, res) => {
+    
+    let query = 'SELECT * FROM main'
+
+// фильтрация
+    let filterOrder = ``
+    let filterTitle = req.query.filterTitle as string;
+    let filterMethod = req.query.filterMethod as string; //'equal' | 'includes' | 'more'| 'lower'
+    let filterValue: number | string = req.query.filterValue as string;
+    
+    if (!!filterTitle && !!filterMethod && !!filterValue) {
+      let method = `LIKE '%${filterValue as string}%'`
+      
+      if (filterTitle === 'count' || filterTitle === 'distance') filterValue = Number(req.query.filterValue)
+      switch (filterMethod) {
+        case  'equal': {
+          method = `= ${(filterTitle == 'name' || filterTitle == 'date') ? `'${filterValue}'` : filterValue}`;
+          break
+        }
+        case  'more': {
+          method = `> ${(filterTitle == 'date') ? `'${filterValue}'` : filterValue}`;
+          break
+        }
+        case  'lower': {
+          method = `< ${(filterTitle == 'date') ? `'${filterValue}'` : filterValue}`;
+          break
+        }
+      }
+      console.log(`${filterTitle} ${method}`)
+      filterOrder = ` WHERE ${filterTitle} ${method}`
+      if (filterTitle === 'date' && filterMethod === 'equal' && !!filterValue) {
+        filterOrder = ` WHERE DATE(date) = '${filterValue}'`
+      }
+      query = query + filterOrder
+      console.log(query)
+    }
+    
+    //не смог красиво сделать условие в sql запросах
+    if (filterTitle === 'date' && filterMethod === 'equal' && !!filterValue) {
+      query = `SELECT * FROM main WHERE DATE(date) = '${filterValue}'`
+      
+    }
+    
+    
+    //сортировка
+    let sortTitle = req.query.sortTitle as string;
+    if (sortTitle) {
+      let sortType = (sortTitle.substring(0, 1) === '1') ? 'ASC' : 'DESC'
+      let sortColumn = sortTitle.substring(1)
+      console.log('sort type :' + sortType)
+      console.log('sort column :' + sortColumn)
+      query = query + ` ORDER BY ${sortColumn} ${sortType}`
+    }
+    
+    //пагинация
+    const currentPage = Number(req.query.currentPage) || 1;
+    const pageSize = Number(req.query.pageSize) || 4;
+    query += ` LIMIT ${pageSize} OFFSET ${(currentPage - 1) * pageSize} `
+    console.log(query)
+    
+    
     // получение с сервера
-    //  let rrr = await db?.query('SELECT * FROM main') || null
+    let totalCount = await client.query(`SELECT COUNT(*) FROM main ${filterOrder}`)
+    console.log(totalCount)
+    
+    console.log(+totalCount.rows[0].count)
+    
+    let rrr = await client.query(query)
     // let books = await db.books.find({}).toArray();
     // const data = await client.query('SELECT * FROM ');
     
-     res.send('ечсть контакт')
-     // res.send(rrr)
+    
+    let data = {
+      pageSize: rrr.rowCount,
+      currentPage: currentPage,
+      items: rrr.rows,
+      totalCount: +totalCount.rows[0].count
+    }
+    res.send(data)
+    // res.send(rrr)
+  })
+  
+  
+  app.post('/', async (req, res) => {
+    
+    let rrr = await client.query('SELECT * FROM main')
+    // let books = await db.books.find({}).toArray();
+    let dataID = 8
+    let dataDate = "2003-01-03T21:00:00.000Z"
+    let dataName = "newName"
+    let dataCount = 199
+    let dataDistance = 122159
+    // await client.query(`INSERT INTO main (id, date, name, count, distance) VALUES (${dataID}, ${dataDate}, ${dataName}, ${dataCount}, ${dataDistance});`);
+    await client.query(`INSERT INTO main ( id, date, name, count, distance) VALUES ( 8, ${new Date()}, "vasya", 199, 100500);`);
+    res.send('row created')
+    // res.send(rrr)
   })
 };
 startApp();
